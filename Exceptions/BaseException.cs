f0ac6ed8787c5168be8cf00718adf477fda7736e
diwsi <xenamorphx@gmail.com>
2023-02-23 22:56:16 +0300

    initial

---
+++ Exceptions/BaseException.cs
@@ -0,0 +1,32 @@
+﻿namespace Exceptions
+{
+    public class BaseException : Exception
+    {
+        public static void ThrowIf(bool argument, string message)
+        {
+            if (argument)
+            {
+                throwException(message);
+            }
+        }
+
+
+        public static void ThrowIfNull(object? argument, string message)
+        {
+            if (argument == null)
+            {
+                throwException(message);
+            }
+        }
+
+        private static void throwException(string message)
+        {
+            throw new(message);
+        }
+
+        public BaseException(string message) : base(message)
+        {
+
+        }
+    }
+}
